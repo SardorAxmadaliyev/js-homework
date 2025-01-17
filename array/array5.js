8c541cbf0c904daa9b2ae56cd504271a40890592
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-17 22:20:54 +0500

    5-masala

---
+++ array/array5.js
@@ -0,0 +1,15 @@
+// let arr = [1, 2, 3];
+
+// if(arr.length < 5) {
+//   console.log("Few elements");
+// }else {
+//   console.log("Many elements");
+// }
+
+let arr = [1, 2, 3, 4, 5];
+
+if(arr.length < 5) {
+  console.log("Few elements");
+}else {
+  console.log("Many elements");
+}
