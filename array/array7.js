3ad0245fe1427f80415547c2f20e3d9c6ada9a4a
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-17 22:36:15 +0500

    7-masala

---
+++ array/array7.js
@@ -0,0 +1,29 @@
+// let arr = [10, 20, 15];
+// let sum = 0;
+
+// for(let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// console.log(`Yig'indi: ${sum}`);
+
+// if(sum < 50) {
+//   console.log( "Sum less than 50");
+// }else {
+//   console.log( "Sum 50 or more");
+// }
+
+let arr = [25, 25, 5];
+let sum = 0;
+
+for(let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+console.log(`Yig'indi: ${sum}`);
+
+if(sum < 50) {
+  console.log( "Sum less than 50");
+}else {
+  console.log( "Sum 50 or more");
+}
