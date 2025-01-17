6c6f9cea39ada4e8f632b7dd0e5fec17c4b7cc07
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-17 18:20:06 +0500

    2-masala

---
+++ array/array2.js
@@ -0,0 +1,29 @@
+// let arr = [-1, -4, -8];
+// let arrayManfiy = true;
+
+// for(let i = 0; i < arr.length; i++) {
+//   if(arr[i] >= 0) {
+//     arrayManfiy = false;
+//   }
+// }
+
+// if(arrayManfiy) {
+//   console.log("All negative");
+// }else {
+//   console.log("At least one non-negative" );
+// }
+
+let arr = [-2, 0, -5];
+let arrayManfiy = true;
+
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] >= 0) {
+    arrayManfiy = false;
+  }
+}
+
+if(arrayManfiy) {
+  console.log("All negative");
+}else {
+  console.log("At least one non-negative" );
+}
