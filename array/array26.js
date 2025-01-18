1b0ca66043d29c4e59b6768372d402c954cc4581
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 22:23:57 +0500

    25-masala to'g'irlandi

--- array/array26.js
+++ array/array26.js
@@ -1,3 +1,30 @@
+// let arr = [2, 5, 8];
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
+
+// let arr = [2, 5, 9];
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
+
+
+// let arr = [1, 2];
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
+
+// to'g'irlangani
+
 let arr = [2, 5, 8, 13];
 
 if (arr.length >= 3) {
