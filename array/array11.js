a4da2e3a8802ccd51871d388b4090331ae567028
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 19:27:41 +0500

    11-masala

---
+++ array/array11.js
@@ -0,0 +1,15 @@
+// let arr = [10, 3, 2, 1];
+
+// if (arr[0] > arr[arr.length - 1]) {
+//   console.log("First bigger");
+// } else {
+//   console.log("Not bigger");
+// }
+
+let arr = [4, 6];
+
+if (arr[0] > arr[arr.length - 1]) {
+  console.log("First bigger");
+} else {
+  console.log("Not bigger");
+}
