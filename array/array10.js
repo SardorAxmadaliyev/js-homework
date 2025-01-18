acdae649e75aa48f5c8aec51f4f0fcf5da25f9e6
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 19:25:02 +0500

    10-masala

---
+++ array/array10.js
@@ -0,0 +1,16 @@
+// let arr = [5];
+
+// if (arr.length === 1) {
+//   console.log("Single element");
+// } else {
+//   console.log("Multiple elements");
+// }
+
+
+let arr = [5, 6];
+
+if (arr.length === 1) {
+  console.log("Single element");
+} else {
+  console.log("Multiple elements");
+}
