2d1b3de5c36664e3a44b0f61318f557d2691b701
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 22:20:53 +0500

    26-masala o'zgartirildi

--- array/array26.js
+++ array/array26.js
@@ -1,24 +1,21 @@
-// let arr = [2, 5, 8];
+let arr = [2, 5, 8, 13];
 
-// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-//   console.log("Arithmetic progression");
-// } else {
-//   console.log("Not arithmetic progression");
-// }
+if (arr.length >= 3) {
+  let check = true;
+  let k = arr[1] - arr[0];
 
-let arr = [2, 5, 9];
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== k) {
+      check = false;
+      break;
+    }
+  }
 
-if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-  console.log("Arithmetic progression");
+  if (check) {
+    console.log("Arithmetic progression");
+  } else {
+    console.log("Not arithmetic progression");
+  }
 } else {
   console.log("Not arithmetic progression");
 }
-
-
-// let arr = [1, 2];
-
-// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-//   console.log("Arithmetic progression");
-// } else {
-//   console.log("Not arithmetic progression");
-// }
