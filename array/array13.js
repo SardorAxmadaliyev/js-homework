995293ab6a5a0d36911d7a32699bb6e8685c3096
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 19:34:55 +0500

    13-masala

---
+++ array/array13.js
@@ -0,0 +1,29 @@
+// let arr = [5, 5, 5];
+
+// let sum = 0;
+// for(let i = 0; i < arr.length; i++) {
+//   sum += arr[i];
+// }
+
+// let average = sum / arr.length;
+
+// if (average >= 5) {
+//   console.log("Average 5 or more");
+// } else {
+//   console.log("Average less than 5");
+// }
+
+let arr =  [2, 4, 5];
+
+let sum = 0;
+for(let i = 0; i < arr.length; i++) {
+  sum += arr[i];
+}
+
+let average = sum / arr.length;
+
+if (average >= 5) {
+  console.log("Average 5 or more");
+} else {
+  console.log("Average less than 5");
+}
