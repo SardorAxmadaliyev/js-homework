0ae10eb3f666ef8a955ca31d15605c94fb560cfa
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 21:09:06 +0500

    25-masala

--- array/array25.js
+++ array/array25.js
@@ -1,44 +1,24 @@
 // let arr = [2, 5, 8];
-// let check = true;
 
 // if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-//   check = true;
-// } else {
-//   check = false;
-// }
-
-// if (check) {
 //   console.log("Arithmetic progression");
 // } else {
 //   console.log("Not arithmetic progression");
 // }
 
-// let arr = [2, 5, 9];
-// let check = true;
-
-// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-//   check = true;
-// } else {
-//   check = false;
-// }
-
-// if (check) {
-//   console.log("Arithmetic progression");
-// } else {
-//   console.log("Not arithmetic progression");
-// }
-
-let arr = [1, 2];
-let check = true;
+let arr = [2, 5, 9];
 
 if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
-  check = true;
-} else {
-  check = false;
-}
-
-if (check) {
   console.log("Arithmetic progression");
 } else {
   console.log("Not arithmetic progression");
 }
+
+
+// let arr = [1, 2];
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
