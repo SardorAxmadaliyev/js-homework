9b2043a1217c17fb787fbdf4fd320384087288ca
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 21:05:32 +0500

    25-masala

---
+++ array/array25.js
@@ -0,0 +1,44 @@
+// let arr = [2, 5, 8];
+// let check = true;
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   check = true;
+// } else {
+//   check = false;
+// }
+
+// if (check) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
+
+// let arr = [2, 5, 9];
+// let check = true;
+
+// if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+//   check = true;
+// } else {
+//   check = false;
+// }
+
+// if (check) {
+//   console.log("Arithmetic progression");
+// } else {
+//   console.log("Not arithmetic progression");
+// }
+
+let arr = [1, 2];
+let check = true;
+
+if (arr[0] < arr[1] && arr[1] < arr[2] && (arr[1] - arr[0] == arr[2] - arr[1])) {
+  check = true;
+} else {
+  check = false;
+}
+
+if (check) {
+  console.log("Arithmetic progression");
+} else {
+  console.log("Not arithmetic progression");
+}
