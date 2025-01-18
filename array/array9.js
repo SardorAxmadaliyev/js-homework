3af428669a66f9e366761474d83515ce0b0411a6
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 19:18:24 +0500

    9-masala

---
+++ array/array9.js
@@ -0,0 +1,32 @@
+// let arr = [0, 1, 2];
+// let check = false;
+
+// for(let i = 0; i < arr.length; i++) {
+//   if(arr[i] === 0) {
+//     check = true;
+//     break;
+//   }
+// }
+
+// if(check) {
+//   console.log("Zero found");
+// }else {
+//   console.log("No zero");
+// }
+
+let arr = [1, 2, 3];
+let check = false;
+
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] === 0) {
+    check = true;
+    break;
+  }
+}
+
+if(check) {
+  console.log("Zero found");
+}else {
+  console.log("No zero");
+}
+
