aba93fe6dae9b416c9d98b760f5cf437f32c45a6
Sardor <axmadaliyevsardor396@gmail.com>
2025-01-18 19:31:43 +0500

    12-masala

---
+++ array/array12.js
@@ -0,0 +1,31 @@
+// let arr = [9, 10, 11];
+// let check = false;
+
+// for(let i = 0; i < arr.length; i++) {
+//   if(arr[i] === 10) {
+//     check = true;
+//     break;
+//   }
+// }
+
+// if(check) {
+//   console.log("Contains 10");
+// }else {
+//   console.log("No 10");
+// }
+
+let arr = [1, 2, 3];
+let check = false;
+
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] === 10) {
+    check = true;
+    break;
+  }
+}
+
+if(check) {
+  console.log("Contains 10");
+}else {
+  console.log("No 10");
+}
